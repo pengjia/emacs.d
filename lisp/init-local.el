aeff60a1f1534ea84ea4b8685de581c0881f518f
Peter <peter.pengjia@gmail.com>
2018-12-13 10:38:10 +0800

    feat(org): do not overwrite the org-refile-targets

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,29 +3,28 @@
 (setq org-default-notes-file (concat org-directory "inbox.org")) ;; capture every things into inbox.org
 (setq org-default-journal-file (concat org-directory "journal.org"))
 (setq org-archive-location (concat org-directory "archive.org::"))
-(setq org-refile-targets '((org-agenda-files :maxlevel . 2)))
 
 (defun s-font()
   (interactive)
   (set-default-font "monospace-14")
   (setq fonts
-        (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
-              ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
-              ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
+		(cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
+			  ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
+			  ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
   (set-face-attribute 'default nil :font
-                      (format "%s:pixelsize=%d" (car fonts) 18))
+					  (format "%s:pixelsize=%d" (car fonts) 18))
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font) charset
-                      (font-spec :family (car (cdr fonts)))))
+	(set-fontset-font (frame-parameter nil 'font) charset
+					  (font-spec :family (car (cdr fonts)))))
   ;; Fix chinese font width and rescale
   (setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro Hei Mono" . 1.2) ("STHeiti". 1.2)))
   )
 (add-to-list 'after-make-frame-functions
-             (lambda (new-frame)
-               (select-frame new-frame)
-               (if window-system (s-font))))
+			 (lambda (new-frame)
+			   (select-frame new-frame)
+			   (if window-system (s-font))))
 (if window-system
-    (s-font))
+	(s-font))
 
 (provide 'init-local)
 ;;;
