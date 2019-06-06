ea784d9064e0537e5519925ea46bdfdd38637ede
Peter <peter.pengjia@gmail.com>
2019-06-07 00:42:45 +0800

    feat(golang): enable auto-complete

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,5 +1,6 @@
 (require-package 'go-mode)
 (require-package 'go-eldoc)
+(require-package 'go-autocomplete)
 (require-package 'golint)
 
 (autoload 'go-mode "go-mode" nil t)
@@ -8,6 +9,7 @@
 (defun go-mode-setup ()
   (setq compile-command "go build -v && go test -v && go vet && golint")
   (go-eldoc-setup)
+  (auto-complete-mode 1)
   (setq gofmt-command "goimports")
   (setq tab-width 4)
   (setq indent-tabs-mode 1)
